643f571da1c51937a5571ceac6ef9744d3245738
Bhavani Shankar <ebhavanishankar@gmail.com>
2017-08-30 13:16:33 +0530

    some updates

--- parse.js
+++ parse.js
@@ -1,3 +1,8 @@
+/*
+Before parsing, remove all whitespace characters from the Json string. 
+So that we don't need to parse the optional spaces again and again.
+*/
+
 function charParser (ch, s) {
   if (s[0] === ch) {
     return [s[0], s.slice(1)]
@@ -50,9 +55,56 @@ const commaParser = charParser.bind(null, ',')
 const colonParser = charParser.bind(null, ':')
 const openSquareBracketParser = charParser.bind(null, '[')
 const closeSquareBracketParser = charParser.bind(null, ']')
-const spaceParser = regexParser.bind(null, /\s+/, null)
-const stringParser = regexParser.bind(null, /"\w+"/, null)
 const numberParser = regexParser.bind(null, /\d+.?\d+e?\d+/, parseFloat)
+
+const stringParser = regexParser.bind(null, /"\w+"/, function (s) {
+  return s.slice(1, s.length - 1)
+})
+
+// define some higher level parsers
+
+function keyParser (s) {
+  // composite of stringParser, colonParser
+  let parsersToTest = [stringParser, colonParser]
+  let result = stringParser(s)
+  if (result === null) {
+    return null
+  }
+
+  let key = result[0]
+  let rest = result[1]
+  let optionalSpacesResult = spaceParser(rest)
+  if (optionalSpacesResult !== null) {
+    rest = optionalSpacesResult[1]
+  }
+
+  result = colonParser(rest)
+  if (result === null) {
+    return null
+  }
+  return [key, result[1]]
+}
+
+function valueParser (s) {
+  let parsersToTest = [nullParser, booleanParser, numberParser, stringParser]
+  for (let parser in parsersToTest) {
+    let result = parsersToTest[parser](s)
+    if (result !== null) {
+      return result
+    }
+  }
+  return null
+}
+
+function arrayParser(s) {
+  
+  return null
+}
+
+// define the json parser
+function jsonParser(s) {
+  
+}
 
 // tests
 let s = '{where'
@@ -90,3 +142,12 @@ console.log(openSquareBracketParser(s))
 
 s = '] something'
 console.log(closeSquareBracketParser(s))
+
+s = '"name"  : "Ra"'
+console.log(keyParser(s))
+
+s = 'null in'
+console.log(valueParser(s))
+
+s = '123 or'
+console.log(valueParser(s))
