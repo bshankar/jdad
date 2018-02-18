52a4fc97c2d07cdccecad987926af26e52f413d9
Bhavani Shankar <ebhavanishankar@gmail.com>
2018-02-18 19:07:14 +0530

    simplify the regular expressions a bit

--- src/parse.js
+++ src/parse.js
@@ -1,11 +1,11 @@
-function regexParser (pattern, s, inner) {
+function reParser (pattern, s, inner) {
   if (!s) return null
   if (typeof s === 'string') s = ['', s]
-  const res = pattern.exec(s[1])
+  const res = pattern.exec(s[1].trim())
   if (res) {
-    const eaten = res[0].trim()
+    const eaten = res[0]
     const len = res[0].length
-    return [inner ? inner(eaten) : eaten, s[1].slice(len)]
+    return [inner ? inner(eaten) : eaten, s[1].trim().slice(len)]
   }
   return null
 }
@@ -16,30 +16,30 @@ function elementParser (acc, s) {
     return val ? [[val[0]], val[1]] : null
   }
   const mbKey = stringParser(s)
-  const mbValue = valueParser(regexParser(/^\s*:/, mbKey))
+  const mbValue = valueParser(reParser(/^:/, mbKey))
   return mbKey && mbValue ? [{[mbKey[0]]: mbValue[0]}, mbValue[1]] : null
 }
 
 function containerParser (open, close, s, acc) {
   const put = (val, acc) => acc instanceof Array
     ? [...acc, ...val] : {...acc, ...val}
-  let res = regexParser(open, s)
+  let res = reParser(open, s)
   while (res) {
     const resElement = elementParser(acc, res)
     if (resElement) acc = put(resElement[0], acc)
-    const hasEnded = regexParser(close, resElement || res)
+    const hasEnded = reParser(close, resElement || res)
     if (hasEnded) return [acc, hasEnded[1]]
-    res = regexParser(/^\s*,/, resElement || res)
+    res = reParser(/^,/, resElement || res)
   }
   return null
 }
 
-const nullParser = s => regexParser(/^\s*null/, s, x => null)
-const boolParser = s => regexParser(/^\s*(?:true|false)/, s, x => x === 'true')
-const numberParser = s => regexParser(/^\s*[-+]?\d+\.?\d*(?:[eE][-+]?\d+)?/, s, parseFloat)
-const stringParser = s => regexParser(/^\s*"(?:\\"|[^"])*"/, s, x => x.slice(1, x.length - 1))
-const arrayParser = s => containerParser(/^\s*\[/, /^\s*\]/, s, [])
-const objectParser = s => containerParser(/^\s*\{/, /^\s*\}/, s, {})
+const nullParser = s => reParser(/^null/, s, x => null)
+const boolParser = s => reParser(/^(?:true|false)/, s, x => x === 'true')
+const numberParser = s => reParser(/^-?\d+\.?\d*(?:[eE][-+]?\d+)?/, s, parseFloat)
+const stringParser = s => reParser(/^"(?:\\"|[^"])*"/, s, x => x.slice(1, -1))
+const arrayParser = s => containerParser(/^\[/, /^\]/, s, [])
+const objectParser = s => containerParser(/^\{/, /^\}/, s, {})
 const valueParser = s => stringParser(s) || numberParser(s) || arrayParser(s) ||
   boolParser(s) || nullParser(s) || objectParser(s)
 
