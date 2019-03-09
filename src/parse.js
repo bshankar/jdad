f7f9ffdaae5057f749daaac7e73bd5b18fc7ab79
Bhavani Shankar E <ebhavanishankar@gmail.com>
2019-03-09 15:20:29 +0530

    Treat arrays as object until the very end

--- src/parse.js
+++ src/parse.js
@@ -6,25 +6,19 @@ function reParser (pattern, s, eval = x => x) {
   return null
 }
 
-function elementParser (acc, s) {
-  if (acc instanceof Array) {
-    const val = valueParser(s)
-    return val ? [[val[0]], val[1]] : null
-  }
-  const mbKey = stringParser(s)
-  const mbValue = valueParser(reParser(/^:/, mbKey))
-  return mbKey && mbValue ? [{[mbKey[0]]: mbValue[0]}, mbValue[1]] : null
+function elementParser (acc, s, isArray) {
+  const key = isArray ? [Object.keys(acc).length, s] : stringParser(s)
+  const value = module.exports.valueParser(isArray ? s : reParser(/^:/, key))
+  return key && value ? [{[key[0]]: value[0]}, value[1]] : null
 }
 
-function containerParser (open, close, s, acc) {
-  const put = (val, acc) => acc instanceof Array
-    ? [...acc, ...val] : {...acc, ...val}
+function containerParser (open, close, s, acc, isArray) {
   let res = reParser(open, s)
   while (res) {
-    const resElement = elementParser(acc, res)
-    if (resElement) acc = put(resElement[0], acc)
+    const resElement = elementParser(acc, res, isArray)
+    if (resElement) acc = { ...acc, ...resElement[0] }
     const hasEnded = reParser(close, resElement || res)
-    if (hasEnded) return [acc, hasEnded[1]]
+    if (hasEnded) return [isArray ? Object.values(acc): acc, hasEnded[1]]
     res = reParser(/^,/, resElement)
     if (res && reParser(close, res)) return null
   }
@@ -35,9 +29,7 @@ const nullParser = s => reParser(/^null/, s, () => null)
 const boolParser = s => reParser(/^(?:true|false)/, s, x => x === 'true')
 const numParser = s => reParser(/^-?\d+\.?\d*(?:[eE][-+]?\d+)?/, s, parseFloat)
 const stringParser = s => reParser(/^"(?:\\"|[^"])*"/, s, x => x.slice(1, -1))
-const arrayParser = s => containerParser(/^\[/, /^\]/, s, [])
-const objectParser = s => containerParser(/^\{/, /^\}/, s, {})
-const valueParser = s => stringParser(s) || numParser(s) || arrayParser(s) ||
-  boolParser(s) || nullParser(s) || objectParser(s)
-
-module.exports = { valueParser }
+const arrayParser = s => containerParser(/^\[/, /^\]/, s, [], true)
+const objectParser = s => containerParser(/^\{/, /^\}/, s, {}, false)
+module.exports.valueParser = s => stringParser(s) || numParser(s) ||
+  arrayParser(s) || boolParser(s) || nullParser(s) || objectParser(s)
